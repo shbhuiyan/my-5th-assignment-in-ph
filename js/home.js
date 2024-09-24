e3a8098ff4db382d4631ed7c6eebaeec0e96b387
Fahad Bhuiyan <shbhuiyan997@gmail.com>
2024-09-24 21:00:29 +0600

    complete assignment

--- js/home.js
+++ js/home.js
@@ -1,5 +1,4 @@
 //********** Donation Section **********
-
 const donationBtn = document
   .getElementById("donation-tab")
   .addEventListener("click", function () {
@@ -13,18 +12,17 @@ const donationBtn = document
       .classList.add("bg-white", "text-gray-500");
 
     classRemove("donation-section");
-    classAdd('history-list')
+    classAdd("history-list");
   });
 
 // Card-1 Button ********
-
 const card1Btn = document
   .getElementById("card-1-btn")
   .addEventListener("click", function () {
     const myDonationAmount = valueCollector("card-1-input");
     const myBalance = innerTextCollector("my-balance");
     const donationAmount = innerTextCollector("total-Donation-Amount-1");
-    const modal = document.getElementById('my_modal_1');
+    const modal = document.getElementById("my_modal_1");
 
     // Error Condition ***
 
@@ -64,21 +62,21 @@ const card1Btn = document
       "mx-auto"
     );
     div.innerHTML = `
-            <p class="capitalize text-lg font-bold">${myDonationAmount} Taka is Donated for ${title1}</p>
+            <p class="text-md font-medium text-amber-500 p-1"><span class="text-black">Donation Amount : </span>${myDonationAmount} BDT</p>
+            <p class="capitalize text-lg font-bold p-1">Donation Fund : <span class="text-gray-600">${title1}</span></p>
             <p class="bg-gray-100 p-1"><span class="font-semibold text-lime-700">Donation Time : </span>${date}</p>
         `;
     document.getElementById("history-list").prepend(div);
   });
 
 // Card-2 Button ********
-
 const card2btn = document
   .getElementById("card-2-btn")
   .addEventListener("click", function () {
     const myDonationAmount = valueCollector("card-2-input");
     const myBalance = innerTextCollector("my-balance");
     const donationAmount = innerTextCollector("total-Donation-Amount-2");
-    const modal =document.getElementById('my_modal_2');
+    const modal = document.getElementById("my_modal_2");
 
     // Error Condition ***
 
@@ -118,14 +116,14 @@ const card2btn = document
       "mx-auto"
     );
     div.innerHTML = `
-            <p class="capitalize text-lg font-bold">${myDonationAmount} Taka is Donated for ${title2}</p>
+            <p class="text-md font-medium text-amber-500 p-1"><span class="text-black">Donation Amount : </span>${myDonationAmount} BDT</p>
+            <p class="capitalize text-lg font-bold p-1">Donation Fund : <span class="text-gray-600">${title2}</span></p>
             <p class="bg-gray-100 p-1"><span class="font-semibold text-lime-700">Donation Time : </span>${date}</p>
         `;
     document.getElementById("history-list").prepend(div);
   });
 
 // Card-3 Button ********
-
 const card3btn = document
   .getElementById("card-3-btn")
   .addEventListener("click", function () {
@@ -172,14 +170,14 @@ const card3btn = document
       "mx-auto"
     );
     div.innerHTML = `
-            <p class="capitalize text-lg font-bold">${myDonationAmount} BDT is Donated for ${title3}</p>
+            <p class="text-md font-medium text-amber-500 p-1"><span class="text-black">Donation Amount : </span>${myDonationAmount} BDT</p>
+            <p class="capitalize text-lg font-bold p-1">Donation Fund : <span class="text-gray-600">${title3}</span></p>
             <p class="bg-gray-100 p-1"><span class="font-semibold text-lime-700">Donation Time : </span>${date}</p>
         `;
     document.getElementById("history-list").prepend(div);
   });
 
 // ************ History Section ***********
-
 const historyBtn = document
   .getElementById("history-tab")
   .addEventListener("click", function () {
@@ -193,13 +191,12 @@ const historyBtn = document
       .classList.add("bg-white", "text-gray-500");
 
     classAdd("donation-section");
-    classRemove('history-list')
+    classRemove("history-list");
   });
 
-
-
 // Blog Button *******
-
-const blogButton = document.getElementById('blog-btn').addEventListener('click',function(){
-  window.location.href ="../blog.html"
-})
+const blogButton = document
+  .getElementById("blog-btn")
+  .addEventListener("click", function () {
+    window.location.href = "../blog.html";
+  });
